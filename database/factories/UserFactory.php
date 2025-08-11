3758af429045b21075eac108d426d8b1651ed44a
Mohammadreza Tavakoli <heyreza8@gmail.com>
2025-08-11 19:58:43 +0330

    add users model, migration, seeder and factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -13,7 +14,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'username' => fake()->userName(),
+            'email' => fake()->safeEmail(),
+            'password' => Hash::make(12345678)
         ];
     }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            //
+            $table->string('username', 255);
+            $table->string('email', 255)->unique();
+            $table->string('password');
             $table->timestamps();
         });
 
